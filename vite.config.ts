0891ef5ab6d68334b3b912a8abb434cb1b471bf5
TechPro369 <aregubq@gmail.com>
2025-02-28 00:05:58 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add this line
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: false, // Disable sourcemaps in production
     rollupOptions: {
       output: {
         manualChunks: {

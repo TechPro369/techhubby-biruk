e683b0e4a91bc0c18ed372adf9f170b319d13e47
TechPro369 <aregubq@gmail.com>
2025-02-28 07:56:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Change from '/' to './'
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: false, // Disable sourcemaps in production
     rollupOptions: {
-      input: {
-        main: './index.html'
-      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-hot-toast'],
@@ -22,10 +18,7 @@ export default defineConfig({
           education: ['./src/components/Education'],
           forms: ['./src/components/ContactForm'],
           modals: ['./src/components/LegalModals']
-        },
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
+        }
       }
     },
     chunkSizeWarningLimit: 1000,
